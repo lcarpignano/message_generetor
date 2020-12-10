7e5af66294be18655159522fc7f3804cd3840f5d
lcarpignano <lcarpignano@gmail.com>
2020-12-10 20:56:31 -0300

    Starting js css and html button

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const path = require ('path');
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.listen(3030, console.log('Ya puedes ir a http://localhost:3030'));
